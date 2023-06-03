ae12461ade2cd3acbce0ed6818927a63ca84a06e
marina-1992 <mariwkabeauty@gmail.com>
2023-06-03 12:04:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -173,3 +173,19 @@
 // }
 
 // console.log(calcTotalPrice(stones, "Щебень"));
+
+// TASK 09
+/**
+ *? Напишіть функцію updateObject, яка приймає об'єкт та ім'я ключа,
+ *? який треба видалити.
+ *? повертає новий об'єкт без вказаного параметра
+ *? Очікуваний результат updateObject({a: 1, b: 2, c: 3}, 'b') => {a: 1, c: 3}
+ *? https://coderoad.ru/208105/%D0%9A%D0%B0%D0%BA-%D1%83%D0%B4%D0%B0%D0%BB%D0%B8%D1%82%D1%8C-%D1%81%D0%B2%D0%BE%D0%B9%D1%81%D1%82%D0%B2%D0%BE-%D0%B8%D0%B7-%D0%BE%D0%B1%D1%8A%D0%B5%D0%BA%D1%82%D0%B0-JavaScript
+ */
+
+const obj = { a: 1, b: 2, c: 3 };
+function updateObject(object, name) {
+ delete object[name]; 
+  return object;
+}
+console.log(updateObject(obj, 'c'));
